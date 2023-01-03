f1d51fa019ecb66fce5000df7015295741bf263c
Phai <norapat_c@cmu.ac.th>
2023-01-03 15:57:09 +0700

    lab11_3 completed

--- lab11_3.cpp
+++ lab11_3.cpp
@@ -1,3 +1,27 @@
-//cout << "Number of data = ";
-//cout << "Mean = ";
-//cout << "Standard deviation = ";
+#include<iostream>
+#include<cmath>
+#include<fstream>
+#include<iomanip>
+#include<string>
+using namespace std;
+
+int main(){
+    ifstream source;
+    source.open("score.txt");
+    string textline;
+    double a,b,d;
+    while(getline(source, textline)){
+        a += atof(textline.c_str());
+        d += pow(atof(textline.c_str()),2);
+        b++;
+    }
+    double mean,sd;
+    mean = a/b;
+    sd = sqrt(d/b-(pow(mean,2)));
+
+
+cout << "Number of data = " << b << endl;
+cout << setprecision(3);
+cout << "Mean = " << mean << endl;
+cout << "Standard deviation = " << sd;
+}
